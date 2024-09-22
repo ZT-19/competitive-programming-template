3312c01a5d32425a918169822459486b61abe931
Zhaorong Tu <64571471+ZT-19@users.noreply.github.com>
2024-09-22 16:25:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+
+// unused stuff
+// const ll INF = 1e9;
+// const ld EPS = 1e-9;
+// #define ar array
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); // fast io
+}
