70d7c7f93e1bff5f5aead1a83573afce11af98a1
Zhaorong Tu <64571471+ZT-19@users.noreply.github.com>
2024-09-29 13:49:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 
-// unused stuff
+// optional (i never use these)
 // const ll INF = 1e9;
 // const ld EPS = 1e-9;
 // #define ar array
